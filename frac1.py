36c1ea93dec2dbfe009e36645321554219dd2999
Malika1109 <68425979+Malika1109@users.noreply.github.com>
2020-07-20 13:55:31 +0300

    definition of get_sierpinski function
    Definition of get_sierpinski function which takes two parameters : length,  which will be the length of side of the base equilateral triangle and level which specifies the depth the sierpinski triangle will display

---
+++ frac1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 19 20:28:57 2020
+
+@author: malika
+"""
+
+
+import turtle
+
+def get_sierpinski(length,level):
+    
+    if level == 1:             #base case
+        for i in range(3):
+            turtle.forward(length)
+            turttle.left(120)
+            
+    else:
+        
+    
+    
+
